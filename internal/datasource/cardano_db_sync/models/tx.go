2e62401a0c7f0cef7397daec11c607867a39c678
Chris Gianelloni <cgianelloni@cloudstruct.net>
2021-12-29 09:52:50 -0500

    Create model: Tx
    Populate the Tx model
    
    - Tx includes transaction information
    
    Signed-off-by: Chris Gianelloni <cgianelloni@cloudstruct.net>

---
+++ internal/datasource/cardano_db_sync/models/tx.go
@@ -0,0 +1,21 @@
+package models
+
+type Tx struct {
+	Id             int64  `gorm:"column:id"`
+	Hash           string `gorm:"column:hash"`           // This is a "hash32type" column
+	BlockId        int64  `gorm:"column:block_id"`       // block(id)
+	BlockIndex     uint32 `gorm:"column:block_index"`
+	OutSum         uint64 `gorm:"column:out_sum"`        // This is a "lovelace" column
+	Fee            uint64 `gorm:"column:fee"`            // This is a "lovelace" column
+	Deposit        int64  `gorm:"column:deposit"`
+	Size           uint32 `gorm:"column:size"`
+	InvalidBefore  string `gorm:"column:invalid_before"` // This is a "word64type" column
+	InvalidAfter   string `gorm:"column:invalid_after"`  // This is a "word64type" column
+	ValidContract  bool   `gorm:"column:valid_contract"`
+	ScriptSize     uint32 `gorm:"column:script_size"`
+}
+
+// Override default pluralized table name
+func (Tx) TableName() string {
+	return "tx"
+}
