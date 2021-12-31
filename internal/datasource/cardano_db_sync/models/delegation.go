bea7599b39fb50a43922a45431e34ea5ba1714b0
Chris Gianelloni <cgianelloni@cloudstruct.net>
2021-12-31 14:59:17 -0500

    Use EpochNumber instead of EpochNo in models
    Signed-off-by: Chris Gianelloni <cgianelloni@cloudstruct.net>

--- internal/datasource/cardano_db_sync/models/delegation.go
+++ internal/datasource/cardano_db_sync/models/delegation.go
@@ -1,14 +1,14 @@
 package models
 
 type Delegation struct {
-	Id            int64  `gorm:"column:id"`
-	AddrId        int64  `gorm:"column:addr_id"` // stake_address(id)
-	CertIndex     int32  `gorm:"column:cert_index"`
-	PoolHashId    int64  `gorm:"column:pool_hash_id"` // pool_hash(id)
-	ActiveEpochNo int64  `gorm:"column:active_epoch_no"`
-	TxId          int64  `gorm:"column:tx_id"` // tx(id)
-	SlotNo        uint32 `gorm:"column:slot_no"`
-	RedeemerId    int64  `gorm:"column:redeemer_id"` // redeemer(id)
+	Id                int64  `gorm:"column:id"`
+	AddrId            int64  `gorm:"column:addr_id"` // stake_address(id)
+	CertIndex         int32  `gorm:"column:cert_index"`
+	PoolHashId        int64  `gorm:"column:pool_hash_id"` // pool_hash(id)
+	ActiveEpochNumber int64  `gorm:"column:active_epoch_no"`
+	TxId              int64  `gorm:"column:tx_id"` // tx(id)
+	SlotNumber        uint32 `gorm:"column:slot_no"`
+	RedeemerId        int64  `gorm:"column:redeemer_id"` // redeemer(id)
 }
 
 // Override default pluralized table name
--- internal/datasource/cardano_db_sync/models/stake_deregistration.go
+++ internal/datasource/cardano_db_sync/models/stake_deregistration.go
@@ -1,12 +1,12 @@
 package models
 
 type StakeDeregistration struct {
-	Id         int64 `gorm:"column:id"`
-	AddrId     int64 `gorm:"column:addr_id"` // stake_address(id)
-	CertIndex  int32 `gorm:"column:cert_index"`
-	EpochNo    int64 `gorm:"column:epoch_no"`
-	TxId       int64 `gorm:"column:tx_id"`       // tx(id)
-	RedeemerId int64 `gorm:"column:redeemer_id"` // redeemer(id)
+	Id          int64 `gorm:"column:id"`
+	AddrId      int64 `gorm:"column:addr_id"` // stake_address(id)
+	CertIndex   int32 `gorm:"column:cert_index"`
+	EpochNumber int64 `gorm:"column:epoch_no"`
+	TxId        int64 `gorm:"column:tx_id"`       // tx(id)
+	RedeemerId  int64 `gorm:"column:redeemer_id"` // redeemer(id)
 }
 
 // Override default pluralized table name
