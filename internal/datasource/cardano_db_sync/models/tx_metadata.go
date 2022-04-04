13cdb8500893099dd89f32913c7df0044ca91e5d
Chris Gianelloni <cgianelloni@cloudstruct.net>
2022-04-04 15:37:23 -0400

    Move jsonb column type from cost_model.go
    Signed-off-by: Chris Gianelloni <cgianelloni@cloudstruct.net>

--- internal/datasource/cardano_db_sync/models/tx_metadata.go
+++ internal/datasource/cardano_db_sync/models/tx_metadata.go
@@ -1,11 +1,11 @@
 package models
 
 type TxMetadata struct {
-	Id             int64  `gorm:"column:id"`
-	Key            string `gorm:"column:key"` // This is a "word64type" column
-	Json           jsonb  `gorm:"column:json"`  // This is a "jsonb" type
-	Bytes          []byte `gorm:"column:bytes"`
-	TxId           int64  `gorm:"column:tx_id"` // tx(id)
+	Id    int64  `gorm:"column:id"`
+	Key   string `gorm:"column:key"`  // This is a "word64type" column
+	Json  jsonb  `gorm:"column:json"` // This is a "jsonb" type
+	Bytes []byte `gorm:"column:bytes"`
+	TxId  int64  `gorm:"column:tx_id"` // tx(id)
 }
 
 // Override default pluralized table name
