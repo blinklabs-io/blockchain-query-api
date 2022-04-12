61e8e36b324ef83e741eaccc0f7f70724ab8c7d8
Chris Gianelloni <cgianelloni@cloudstruct.net>
2022-04-12 16:39:38 -0400

    Refactor datasource(s)
    Split the cardano_db_sync datasource into different packages.
    - Rename jsonb to Jsonb
    - Create postgres/types and move Jsonb here
    - Create koios/models and move Koios-related models here
    
    Signed-off-by: Chris Gianelloni <cgianelloni@cloudstruct.net>

--- internal/datasource/cardano_db_sync/models/tx_metadata.go
+++ internal/datasource/cardano_db_sync/models/tx_metadata.go
@@ -1,11 +1,15 @@
 package models
 
+import (
+	"github.com/cloudstruct/blockchain-query-api/internal/datasource/postgres/types"
+)
+
 type TxMetadata struct {
-	Id    int64  `gorm:"column:id"`
-	Key   string `gorm:"column:key"`  // This is a "word64type" column
-	Json  jsonb  `gorm:"column:json"` // This is a "jsonb" type
-	Bytes []byte `gorm:"column:bytes"`
-	TxId  int64  `gorm:"column:tx_id"` // tx(id)
+	Id    int64       `gorm:"column:id"`
+	Key   string      `gorm:"column:key"`  // This is a "word64type" column
+	Json  types.Jsonb `gorm:"column:json"` // This is a "jsonb" type
+	Bytes []byte      `gorm:"column:bytes"`
+	TxId  int64       `gorm:"column:tx_id"` // tx(id)
 }
 
 // Override default pluralized table name
